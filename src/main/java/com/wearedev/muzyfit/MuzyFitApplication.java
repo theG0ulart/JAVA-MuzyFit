61adafd7a9e7e297ca4e2c73615d7272f106e74e
Lucas Goulart <lucas.goulart@wearedev.com.br>
2024-07-10 19:12:40 -0300

    :tada: Início projeto MuzyFit - API para Cadastro de Alunos e Avaliações Físicas dos mesmos.

---
+++ src/main/java/com/wearedev/muzyfit/MuzyFitApplication.java
@@ -0,0 +1,13 @@
+package com.wearedev.muzyfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuzyFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuzyFitApplication.class, args);
+	}
+
+}
